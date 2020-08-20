e5ffe5978986604d045fe57bb107e8da9aaa23fc
anupsingh2008 <anup4ever28@gmail.com>
2020-08-20 21:33:49 +0530

    Add project files.

---
+++ DisignPattern/CreationalPattern/AbstractFactory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DesignPattern.DisignPattern.CreationalPattern
+{
+    abstract class AbstractContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+
+    class AfricaFactory : AbstractContinentFactory
+    {
+        public override Carnivore CreateCarnivore()
+        {
+             return new Lion();
+        }
+
+        public override Herbivore CreateHerbivore()
+        {
+ 
+            return new WildBeast();
+        }
+    }
+    class AmericaFactory : AbstractContinentFactory
+    {
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
+        }
+
+        public override Herbivore CreateHerbivore()
+        {
+            return new Bison();
+        }
+    }
+
+    internal class WildBeast : Herbivore
+    {
+    }
+
+    internal class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine($"{this.GetType().Name} eats {h.GetType().Name}");
+        }
+    }
+
+    internal class Bison : Herbivore
+    {
+
+    }
+
+    internal class Wolf : Carnivore 
+    {
+        public override void Eat(Herbivore h)
+        {
+            Console.WriteLine($"{this.GetType().Name} eats {h.GetType().Name}");
+        }
+    }
+
+    abstract class Carnivore
+    {
+        public abstract void Eat(Herbivore h);
+    }
+
+    abstract class Herbivore
+    {
+        
+    }
+
+    class AnimalWorld
+    {
+        private Herbivore _herbivore;
+        private Carnivore _Carnivore;
+        public AnimalWorld(AbstractContinentFactory abstractContinentFactory)
+        {
+            _herbivore = abstractContinentFactory.CreateHerbivore();
+            _Carnivore = abstractContinentFactory.CreateCarnivore();
+
+        }
+
+        public void RunFoodChain()
+        {
+            _Carnivore.Eat(_herbivore);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using DesignPattern.DisignPattern.CreationalPattern;
+using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Design Pattern Name");
+            Console.WriteLine("Enter abs for Abstract Factory Method.");
+            string str = Console.ReadLine();
+            if (str.ToLower() == "abs")
+            {
+                AbstractContinentFactory continentFactory = new AfricaFactory();
+                AnimalWorld animalWorld = new AnimalWorld(continentFactory);
+                animalWorld.RunFoodChain();
+
+                AbstractContinentFactory continentFactory1 = new AmericaFactory();
+                AnimalWorld animalWorld1 = new AnimalWorld(continentFactory1);
+                animalWorld1.RunFoodChain();
+            }
+            Console.ReadKey();
+        }
+    }
+}
